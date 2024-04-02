89c9e1ac7b8544e7b100d2323dd4fa7a8e46b82e
Ezziber Salma <ezzibersalma@gmail.com>
2024-04-02 03:01:26 +0200

    Initial commit

---
+++ src/test/java/ma/emsi/university/UniversityApplicationTests.java
@@ -0,0 +1,15 @@
+package ma.emsi.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApplicationTests {
+
+
+    public static void main (String[] args)     {
+            SpringApplication.run(UniversityApplicationTests.class, args);
+    }
+
+}
